153d8473726211a237b41275a9da2569be4538fc
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-02-08 14:30:57 -0300

    feat: add google adsense

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AdsenseModule } from 'ng2-adsense';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AdsenseModule.forRoot({
+      adClient: 'ca-pub-3401711569005699',
+      adSlot: 7259870550,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
