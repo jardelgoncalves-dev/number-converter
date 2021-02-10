31de4db2b889112c91168e355d61dce5fe5f3bb7
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-02-10 00:03:54 -0300

    feat: add methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { convert } from '../util/convertNumber'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'conversions';
+  converterType = 'dollar'
+  result = ''
+  number = ''
+
+  onChangeConverterType(type: string): void {
+    this.converterType = type
+  }
+
+  onConvert() {
+    this.result = convert(this.number, this.converterType === 'dollar')
+  }
 }
